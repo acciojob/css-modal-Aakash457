86d05ed042cfbab8c8a9af2ddd249b4189be23e2
Aakash457 <114485477+Aakash457@users.noreply.github.com>
2026-02-08 13:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const openBtn = document.getElementById("openModal");
-const modal = document.querySelector(".modal");
-const closeBtn = document.querySelector(".close-modal");
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.getElementById("openModal");
+  const modal = document.querySelector(".modal");
+  const closeBtn = document.querySelector(".close-modal");
 
-openBtn.addEventListener("click", () => {
-  modal.style.display = "block";
-});
+  openBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
 
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
+  closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
-  }
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 });
